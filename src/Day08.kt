0343d89b0034047ce0b345b9039f868879d5df1b
Claus Rasmussen <chr@lunar.app>
2022-12-10 12:47:28 +0100

    Added some days worth.. Try

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,22 +1,155 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+typealias Column = List<Int>
+typealias Row = List<Int>
+
+data class TreeGrid(val rows: List<Row>, val columns: List<Column>)
+
+fun mapToTreeGrid(input: List<String>): TreeGrid {
+    val rows = mutableListOf<List<Int>>()
+    val columns = mutableListOf<List<Int>>()
+
+    // Rows
+    input.forEach { horizontal ->
+        rows.add(
+            horizontal.map {
+                it.digitToInt()
+            })
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    for (columnIndex in input.indices) {
+        columns.add(rows.map {
+            it[columnIndex]
+        })
     }
+
+    return TreeGrid(rows, columns)
+}
+
+private fun calculateVisibleTrees(treeGrid: TreeGrid): Int {
+    val coordinateSet = mutableSetOf<Pair<Int, Int>>()
+    // Get borders
+    treeGrid.rows.forEachIndexed outer@{ y, row ->
+        if (y == 0 || y == treeGrid.rows.size - 1) {
+            coordinateSet.addAll(List(row.size) { x -> Pair(x, y) })
+        } else {
+//            //Do actual calculations thing
+//            //We need to traverse both from right and left (reversed)
+            var currentMax = row.first()
+            row.forEachIndexed inner@{ x, tree ->
+                if (tree > currentMax) {
+                    currentMax = tree
+                    coordinateSet.add(Pair(x, y))
+                } else {
+                    return@inner
+                }
+            }
+
+            // Swap directions
+            val reversedRow = row.reversed()
+            currentMax = reversedRow.first()
+
+            reversedRow.forEachIndexed { x, tree ->
+                if (tree > currentMax) {
+                    currentMax = tree
+                    coordinateSet.add(Pair((reversedRow.size - 1) - x, y))
+                } else {
+                    return@forEachIndexed
+                }
+            }
+        }
+    }
+
+    treeGrid.columns.forEachIndexed outer@{ x, column ->
+        if (x == 0 || x == treeGrid.columns.size - 1) {
+            coordinateSet.addAll(List(column.size) { y -> Pair(x, y) })
+        } else {
+            //Do actual calculations thing
+            //We need to traverse both from top and bottom (reversed)
+            var currentMax = column.first()
+            column.forEachIndexed { y, tree ->
+                if (tree > currentMax) {
+                    currentMax = tree
+                    coordinateSet.add(Pair(x, y))
+                } else {
+                    return@forEachIndexed
+                }
+            }
+
+            // Reset / Reverse
+            val reversedColumn = column.reversed()
+            currentMax = reversedColumn.first()
+            reversedColumn.forEachIndexed { y, tree ->
+                if (tree > currentMax) {
+                    currentMax = tree
+                    coordinateSet.add(Pair(x, (reversedColumn.size - 1) - y))
+                } else {
+                    return@forEachIndexed
+                }
+            }
+        }
+    }
+
+    return coordinateSet.count()
+}
+
+/**
+ * stop if you reach an edge or at the first tree that is the same height or taller than the tree under consideration
+ * (If a tree is right on the edge, at least one of its viewing distances will be zero.)
+ */
+private lateinit var grid: List<List<Int>>
+
+private fun treeScore(x: Int, y: Int): Int {
+    val treeHeight = grid[x][y]
+    val row = grid[x]
+    if (x == 0 || x == row.size - 1 || y == 0 || y == grid.size - 1) {
+        return 0
+    }
+
+    fun getScore(treeLine: List<Int>): Int {
+        var count = 0
+        for (tree in treeLine) {
+            count++
+            if (tree >= treeHeight) {
+                break
+            }
+        }
+        return count
+    }
+
+    val left = getScore(row.subList(0, y).reversed())
+    val right = getScore(row.subList(y + 1, grid.size))
+    val up = getScore(grid.map { it[y] }.subList(0, x).reversed())
+    val down = getScore(grid.map { it[y] }.subList(x + 1, grid.size))
+
+    return left * right * up * down
+}
+
+private fun findScenicHighScore(): Int {
+    return grid.indices.maxOf { x ->
+        grid[x].indices.maxOf { y ->
+            treeScore(x, y)
+        }
+    }
+}
+
+fun main() {
+    val input = readInput("Day08")
+
+    grid = input.map { it.map { it.digitToInt() } }
+
+    fun part1(input: List<String>): Int = calculateVisibleTrees(mapToTreeGrid(input))
+
+    fun part2(): Int = findScenicHighScore()
 
     //Enter test values / types here
-    val testAnswer1: Int? = null
-    val testAnswer2: Int? = null
+    val testAnswer1 = 21
+    val testAnswer2 = 8
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day08_test")
-    testAnswer1?.let { check(part1(testInput) == it) }
-    testAnswer2?.let { check(part2(testInput) == it) }
 
-    val input = readInput("Day08")
+    check(part1(testInput) == testAnswer1)
+    check(part2() == testAnswer2)
+
     println(part1(input))
-    println(part2(input))
+    println("part 2: ${part2()}")
 }
---
+++ src/Day09.kt
@@ -0,0 +1,85 @@
+import kotlin.math.abs
+
+private fun part1(input: List<String>): Int {
+    val movement = input.map(::movement)
+    return findUniqueTailLocations(movement, children = 1)
+}
+
+private fun part2(input: List<String>): Int {
+    val movement = input.map(::movement)
+    return findUniqueTailLocations(movement, children = 9)
+}
+
+private fun findUniqueTailLocations(movements: List<Movement>, children: Int = 1): Int {
+    val rope = (0..children).map { Location(0, 0) }.toMutableList()
+    val lastVisitedTailLocation = mutableSetOf<Location>().also { it.add(rope.last()) }
+
+    movements.forEach { movement ->
+        repeat(movement.steps) {
+            //Move head
+            rope[0] = rope[0].move(movement.direction)
+
+            var parentIndex = 0
+            var shouldCheckChild = true
+            // move child
+            while (shouldCheckChild) {
+                val parent = rope[parentIndex]
+                val child = rope[parentIndex + 1]
+
+                if (abs(parent.x - child.x) > 1 || abs(parent.y - child.y) > 1) {
+                    rope[parentIndex + 1] = Location(
+                        x = child.x + parent.x.compareTo(child.x),
+                        y = child.y + parent.y.compareTo(child.y)
+                    ).also {
+                        if (parentIndex + 1 == children) {
+                            lastVisitedTailLocation.add(it)
+                            shouldCheckChild = false
+                        }
+                    }
+                } else {
+                    shouldCheckChild = false
+                }
+
+                parentIndex++
+            }
+        }
+    }
+
+    println("Size ${lastVisitedTailLocation.size}")
+    return lastVisitedTailLocation.size
+}
+
+
+fun main() {
+    val input = readInput("Day09")
+    println(part1(input))
+    println(part2(input))
+}
+
+private fun movement(line: String) = line.split(" ").let { (d, s) ->
+    val direction = when (d) {
+        "R" -> Direction.Right
+        "L" -> Direction.Left
+        "U" -> Direction.Up
+        "D" -> Direction.Down
+        else -> throw IllegalArgumentException("Invalid direction")
+    }
+    val steps = s.toInt()
+    Movement(direction, steps)
+}
+
+data class Movement(val direction: Direction, val steps: Int)
+enum class Direction {
+    Left, Right, Up, Down
+}
+
+data class Location(val x: Int, val y: Int) {
+    fun move(direction: Direction): Location {
+        return when (direction) {
+            Direction.Left -> Location(x - 1, y)
+            Direction.Right -> Location(x + 1, y)
+            Direction.Up -> Location(x, y + 1)
+            Direction.Down -> Location(x, y - 1)
+        }
+    }
+}
